42346df86bbc9fca406c0ec994cb00794fbe6aec
Error4ik_826 <78443482+error4ikLOH@users.noreply.github.com>
2023-10-01 22:51:28 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,7 @@
+# Токен вашого бота з BotFather в Telegram
+TOKEN = '6218543158:AAEoHCPJ1TXCCa9aqhG7HQu8GAdxEhxLGdI'
+
+# Назви команд, які бот може розпізнавати (за вашим описом, це "!я", "!кубик", "!ставка")
+COMMAND_YA = '!я'
+COMMAND_CUBE = '!кубик'
+COMMAND_BET = '!ставка'
